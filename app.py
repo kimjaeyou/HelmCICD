198e081b1ab1b1e52f3e71f3422d77372f139a82
kimjaeyou <unknown88455144@gmail.com>
2025-06-16 19:55:12 +0900

    test55

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def index():
             cur.execute("SELECT count FROM visits")
             visit_count = cur.fetchone()[0]
         db.close()
-        return f"Flask App has been visited {visit_count} times 19:50!"
+        return f"Flask App has been visited {visit_count} times 19:55!"
     except Exception as e:
         return f"Error: {str(e)}", 500
 
